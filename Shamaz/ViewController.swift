d835b19fab802f5ecdf9b312d5e10e44b15c68e5
Ellen Covey <covey.ellen@gmail.com>
2019-05-08 16:24:47 +0100

    Initial Commit

---
+++ Shamaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shamaz
+//
+//  Created by Ellen Covey on 08/05/2019.
+//  Copyright © 2019 Ellen Covey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
