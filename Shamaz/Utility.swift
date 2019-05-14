0726f1946c03d09b2290a1d91b66a2c71c18cc37
Ellen Covey <covey.ellen@gmail.com>
2019-05-14 12:01:15 +0100

    First full commit

--- Shamaz/Utility.swift
+++ Shamaz/Utility.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class Utility {
+    
+    static var reflectActions = ["Describe your day", "Where were you", "Who did you talk to", "How were you feeling"]
+    static var reflectTimes = ["two days ago", "yesterday", "last Saturday", "last night"]
+    static var dreamActions = ["Where do you want to go", "What will you be doing", "Who would you like to be with", "What are your plans for"]
+    static var dreamTimes = ["tomorrow", "this time next year", "next Friday", "a month from now"]
+    
+    static func generateReflectAction() -> String {
+        return reflectActions.randomElement()!
+    }
+    
+    static func generateReflectTime() -> String {
+        return reflectTimes.randomElement()!
+    }
+    
+    static func generateDreamAction() -> String {
+        return dreamActions.randomElement()!
+    }
+    
+    static func generateDreamTime() -> String {
+        return dreamTimes.randomElement()!
+    }
+    
+}
--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -2,19 +2,63 @@
 //  ViewController.swift
 //  Shamaz
 //
-//  Created by Ellen Covey on 08/05/2019.
+//  Created by Ellen Covey on 07/05/2019.
 //  Copyright © 2019 Ellen Covey. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let defaultColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    let highlightColor = UIColor(red: 0.0, green: 0.478, blue: 1.0, alpha: 1.0)
+    let deactivatedColor = UIColor(red: 0.0, green: 0.478, blue: 1.0, alpha: 0.3)
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var reflectButton: RoundButton!
+    @IBOutlet weak var dreamButton: RoundButton!
+    @IBOutlet weak var nextButton: RoundButton!
+    // Code here
+    
+    @IBAction func generateReflection() {
+        let reflectAction = Utility.generateReflectAction()
+        let reflectTime = Utility.generateReflectTime()
+        message.text = "\(reflectAction)" + " \(reflectTime)?"
+        message.textColor = defaultColor
+        toggleButtons(turnedOn: true)
     }
-
-
+    
+    @IBAction func generateDream(_ sender: Any) {
+        let dreamAction = Utility.generateDreamAction()
+        let dreamTime = Utility.generateDreamTime()
+        message.text = "\(dreamAction)" + " \(dreamTime)?"
+        message.textColor = defaultColor
+        toggleButtons(turnedOn: true)
+    }
+    
+    @IBAction func generatePerson(_ sender: Any) {
+        let playerNumber = Int.random(in: 1...10)
+        message.text = "It's player \(playerNumber)'s turn"
+        message.textColor = highlightColor
+        toggleButtons(turnedOn: false)
+    }
+    
+    func toggleButtons(turnedOn: Bool) {
+        nextButton.isEnabled = turnedOn
+        reflectButton.isEnabled = !turnedOn
+        dreamButton.isEnabled = !turnedOn
+        
+        if reflectButton.isEnabled == false {
+            reflectButton.backgroundColor = deactivatedColor
+            dreamButton.backgroundColor = deactivatedColor
+            nextButton.backgroundColor = highlightColor
+        }
+            
+        else {
+            reflectButton.backgroundColor = highlightColor
+            dreamButton.backgroundColor = highlightColor
+            nextButton.backgroundColor = deactivatedColor
+        }
+        
+    }
 }
-
