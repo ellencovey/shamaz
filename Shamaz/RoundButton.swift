5db8f707fe8f5efc2b0a1796f0b84c96a6151004
Ellen Covey <covey.ellen@gmail.com>
2019-05-14 11:57:19 +0100

    First commit

---
+++ Shamaz/RoundButton.swift
@@ -0,0 +1,9 @@
+//
+//  RoundButton.swift
+//  Shamaz
+//
+//  Created by Ellen Covey on 08/05/2019.
+//  Copyright © 2019 Ellen Covey. All rights reserved.
+//
+
+import Foundation
---
+++ Shamaz/Utility.swift
@@ -0,0 +1,9 @@
+//
+//  Utility.swift
+//  Shamaz
+//
+//  Created by Ellen Covey on 09/05/2019.
+//  Copyright © 2019 Ellen Covey. All rights reserved.
+//
+
+import Foundation
